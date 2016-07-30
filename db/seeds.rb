e0f8becff0c38e6785324bb73bb8f1d64a25e55a
Matt T <code.matthew.thompson@gmail.com>
2016-07-29 22:20:59 -0400

    Switching to parsing the actual game data instead of scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,10 @@ require 'json'
 require 'pry'
 
 def seed_all_the_things!
-  # CSV.foreach("#{Rails.root}/db/pokemon_data.csv") do |pokemon|
-  #   Pokemon.create(
-  #     poke_id: pokemon[0],
-  #     name: pokemon[1],
-  #     stamina: pokemon[4],
-  #     attack: pokemon[5],
-  #     defence: pokemon[6],
-  #     description: "placeholder for description",
-  #     image: "placeholder for image"
-  #   )
-  # end
   file = File.read('GAME_MASTER_v0_1.decoded.categorized.json')
   data = JSON.parse(file)
   pokemon = scan_pokemons(data)
   moves = scan_moves(data)
-  types = scan_types(data)
-  binding.pry
 end
 
 def scan_pokemons(data)
