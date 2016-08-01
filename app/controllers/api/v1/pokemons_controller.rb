52b2d487991a181fa4d462a87eec56c77af402a3
Matt T <code.matthew.thompson@gmail.com>
2016-08-01 11:37:29 -0400

    working on api for pokemon first

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,5 +1,39 @@
 class Api::V1::PokemonsController < ActionController::Base
+  VALID_QUERIES = ['name','attack','stamina','defence','min_trainer_level','type']
+
   def index
-    render json: Pokemon.all
+    unless validate_queries(request.GET.keys)
+      render json: {error: "Bad Query"}
+      return
+    end
+
+    #still need to sanitize this and also
+    #change name to LIKE instead of ==
+    render json: Pokemon.where(build_result(request))
+  end
+
+  def build_result(request)
+    query_str = ""
+    i = 0
+    request.GET.each do |query,value|
+      i += 1
+      case query
+        when "name"
+          query_str += "name = '#{value}'"
+        when "attack"
+          query_str += "attack > #{value}"
+        when "defence"
+          query_str += "defence > #{value}"
+      end
+      query_str += " AND " unless i == request.GET.keys.length
+    end
+    query_str
+  end
+
+  def validate_queries(queries)
+    queries.each do |query|
+      return false unless VALID_QUERIES.include?(query)
+    end
+    true
   end
 end
