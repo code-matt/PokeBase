c79b38c9171272983079eef0693b57995c083ae7
Matt T <code.matthew.thompson@gmail.com>
2016-08-07 01:56:26 -0400

    got rid of type donut :( moved picture over to stats card component

--- Angular2_frontend/app/pane/stats/stats.component.js
+++ Angular2_frontend/app/pane/stats/stats.component.js
@@ -13,6 +13,7 @@ var Observable_1 = require('rxjs/Observable');
 require('rxjs/add/observable/timer');
 var card_1 = require('@angular2-material/card');
 var donut_types_component_1 = require('../donut-types/donut-types.component');
+var type_badge_component_1 = require('./type-badge/type-badge.component');
 var PokemonStatsComponent = (function () {
     function PokemonStatsComponent() {
         this.pokemon = {};
@@ -37,8 +38,8 @@ var PokemonStatsComponent = (function () {
         core_1.Component({
             selector: "pokemon-stats",
             styleUrls: ['app/pane/stats/stats.component.css'],
-            directives: [card_1.MD_CARD_DIRECTIVES, donut_types_component_1.DonutTypesComponent],
-            template: "\n  <div #stats *ngIf=\"pokemon.types\" class=\"stats\">\n    <md-card class=\"shadowfilter\">\n     <md-card-content>\n       <donut-types class=\"donut\" [pokemon]=\"pokemon\"></donut-types>\n       <h2>{{pokemon.name}}</h2>\n       <p>Attack: {{pokemon.attack}}</p>\n       <p>Defence: {{pokemon.defence}}</p>\n       <p>Stamina: {{pokemon.stamina}}</p>\n     </md-card-content>\n     <hr/>\n    </md-card>\n  </div>\n  "
+            directives: [card_1.MD_CARD_DIRECTIVES, donut_types_component_1.DonutTypesComponent, type_badge_component_1.TypeBadgeComponent],
+            template: "\n  <div #stats *ngIf=\"pokemon.types\" class=\"stats\">\n    <md-card class=\"shadowfilter\">\n      <img class=\"img-responsive shadowfilter\" src=\"http://localhost:8000/images/full/{{pokemon.poke_id}}.png\">\n      <md-card-content>\n        <h2>{{pokemon.name}}</h2>\n        <p>Attack: {{pokemon.attack}}</p>\n        <p>Defence: {{pokemon.defence}}</p>\n        <p>Stamina: {{pokemon.stamina}}</p>\n      </md-card-content>\n      <hr/>\n      <type-badge *ngFor=\"let type of pokemon.types\" [type]=\"type.name\"></type-badge>\n    </md-card>\n  </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], PokemonStatsComponent);
--- Angular2_frontend/app/pane/stats/stats.component.ts
+++ Angular2_frontend/app/pane/stats/stats.component.ts
@@ -3,22 +3,24 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card'
 import {DonutTypesComponent} from '../donut-types/donut-types.component'
+import {TypeBadgeComponent} from './type-badge/type-badge.component';
 
 @Component({
   selector: `pokemon-stats`,
   styleUrls: ['app/pane/stats/stats.component.css'],
-  directives: [MD_CARD_DIRECTIVES, DonutTypesComponent],
+  directives: [MD_CARD_DIRECTIVES, DonutTypesComponent, TypeBadgeComponent],
   template: `
   <div #stats *ngIf="pokemon.types" class="stats">
     <md-card class="shadowfilter">
-     <md-card-content>
-       <donut-types class="donut" [pokemon]="pokemon"></donut-types>
-       <h2>{{pokemon.name}}</h2>
-       <p>Attack: {{pokemon.attack}}</p>
-       <p>Defence: {{pokemon.defence}}</p>
-       <p>Stamina: {{pokemon.stamina}}</p>
-     </md-card-content>
-     <hr/>
+      <img class="img-responsive shadowfilter" src="http://localhost:8000/images/full/{{pokemon.poke_id}}.png">
+      <md-card-content>
+        <h2>{{pokemon.name}}</h2>
+        <p>Attack: {{pokemon.attack}}</p>
+        <p>Defence: {{pokemon.defence}}</p>
+        <p>Stamina: {{pokemon.stamina}}</p>
+      </md-card-content>
+      <hr/>
+      <type-badge *ngFor="let type of pokemon.types" [type]="type.name"></type-badge>
     </md-card>
   </div>
   `
